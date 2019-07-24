fa87595a994fee5d56e50e78520e37892baa6576
Ben Brunton <benbrunton@hotmail.com>
2019-07-24 21:27:43 +0100

    bit o colour

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,30 @@ use std::env;
 const CD_DEFAULT: &str = "~";
 
 fn main() {
+    println!("Brush - the \x1b[1;33mBR\x1b[0munton \x1b[1;33mSH\x1b[0mell");
     loop {
         print!("> ");
         stdout().flush().unwrap();
+        
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
+        // read_to_string 
+        //      requires the std::io::Read trait to be imported
+
+        // some whitespace characters need handling
+        // but this will require reading from raw input
+        // e.g.  clears screen in bash
+        // ^[[A retrieves previous command
+        // println!("{:?}", input);
+        // outputting a \r allows overwrite of lines
 
         let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
+        let command_option = parts.next();
+        let command = command_option.unwrap_or("");
         let args = parts;
 
         match command {
+            "" => (),
             "cd" => {
                 let new_dir = args.peekable()
                     .peek()
@@ -33,7 +46,9 @@ fn main() {
                     .spawn();
 
                 match child {
-                    Ok(mut child) => { child.wait().unwrap(); },
+                    Ok(mut child) => { 
+                        let _ = child.wait(); 
+                    },
                     Err(e) => eprintln!("{}", e)
                 };
             }
